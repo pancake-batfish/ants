f56ccd6fc140ab21bc2151b3c6cf7235d2119274
Regan Huff <regan.huff@gmail.com>
2017-07-10 09:35:28 -0700

    Wander first pass

--- ant.js
+++ ant.js
@@ -1,8 +1,11 @@
 function Ant(x, y) {
   this.pos = createVector(x, y);
-  this.vel = createVector(1, 1);
+  this.vel = createVector(0.8, 0.8);
   this.acc = createVector(0,0);
+  this.maxspeed = 1;
+  this.maxforce = 0.2;
   this.d = 5;
+  this.t = 0;
 
   this.run = function(ants) {
     //TODO: add function to apply forces
@@ -12,10 +15,12 @@ function Ant(x, y) {
   };
 
   this.applyForce = function(force) {
-    this.acc.add(f);
+    this.acc.add(force);
   };
 
   this.update = function() {
+      // var wandering = this.wander();
+      // this.applyForce(wandering);
       this.vel.add(this.acc);
       this.pos.add(this.vel);
       this.acc.set(0, 0);
@@ -44,5 +49,14 @@ function Ant(x, y) {
     //use noise to set a new direction
     //different from but related to current direction
 
-  }
+    var x = noise(this.t);
+    var y = noise(this.t);
+    this.t += .01;
+    var noiseVector = createVector(x,y); //will generate with noise
+    var desired = p5.Vector.add(noiseVector, this.pos);
+    desired.setMag(this.maxspeed);
+    var steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxforce);
+    return steer;
+  };
 }
