e946eb57ab577d71863009c62a0a51142b86669b
Regan Huff <regan.huff@gmail.com>
2017-07-18 13:53:46 -0700

    Move ant and nest creation to colony.js

--- colony.js
+++ colony.js
@@ -1,13 +1,41 @@
 function Colony() {
+  var nest;
   this.ants = [];
+
+  this.establish = function() {
+    //create nest
+    // var nestPosition = createVector(100, height/6);
+    var nestPosition = createVector(width/2, height/2);
+    var nestDiameter = 200;
+    var nestRotation = 45;
+    nest = new Nest(nestPosition.x, nestPosition.y, nestDiameter, radians(nestRotation));
+
+    //ants anywhere
+    // for (var i = 0; i < 15; i++) {
+    //   var ant = new Ant(random(width), random(height), nest);
+    //   this.ants.push(ant);
+    // }
+
+    //ants in nest
+    // push();
+    // translate(nest.position.x, nest.position.y);
+    for (var i = 0; i < 7; i++) {
+      var randomLocation = nest.locationInNest();
+      // console.log("colony:" + randomLocation.x);
+      var ant = new Ant(randomLocation.x, randomLocation.y, nest);
+      this.ants.push(ant);
+    }
+    // pop();
+  };
 
   this.run = function() {
     for (var i = 0; i < this.ants.length; i++) {
       this.ants[i].run(this.ants);
     }
+    nest.render();
   };
 
-  this.addAnt = function(ant) {
-    this.ants.push(ant);
-  }
+  // this.addAnt = function(ant) {
+  //   this.ants.push(ant);
+  // }
 }
--- sketch.js
+++ sketch.js
@@ -1,46 +1,47 @@
 var colony;
-var nest;
+// var nest;
 var supply = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colony = new Colony();
-  var nestPosition = createVector(100, height/6);
-  var nestDiameter = 200;
-  var nestRotation = 45;
-  nest = new Nest(nestPosition.x, nestPosition.y, nestDiameter, radians(nestRotation));
+  colony.establish();
+  // var nestPosition = createVector(100, height/6);
+  // var nestDiameter = 200;
+  // var nestRotation = 45;
+  // nest = new Nest(nestPosition.x, nestPosition.y, nestDiameter, radians(nestRotation));
 
   //distribution of food
   for (var i = 0; i < 15; i++) {
     var food = new Food(random(width), random(height));
-    if (food.pos.dist(nest.pos) > (nest.radius)) {
+    // if (food.pos.dist(nest.pos) > (nest.radius)) {
       supply.push(food);
-    }
+    // }
   }
   //ants anywhere
-  for (var i = 0; i < 15; i++) {
-    var ant = new Ant(random(width), random(height), nest);
-    colony.addAnt(ant);
-  }
-  //ants inside nest
-  var j = 0;
-  while (j < 7) {
-    push();
-    translate(nest.pos.x, nest.pos.y);
-    var ant = new Ant(random(-nest.radius, nest.radius), random(-nest.radius, nest.radius), nest);
-    pop();
-    if (ant.insideNest(ant.pos)) {
-      colony.addAnt(ant);
-      j++;
-    }
-  }
+  // for (var i = 0; i < 15; i++) {
+  //   var ant = new Ant(random(width), random(height), nest);
+  //   colony.addAnt(ant);
+  // }
+  // //ants inside nest
+  // var j = 0;
+  // while (j < 7) {
+  //   push();
+  //   translate(nest.pos.x, nest.pos.y);
+  //   var ant = new Ant(random(-nest.radius, nest.radius), random(-nest.radius, nest.radius), nest);
+  //   pop();
+  //   if (ant.insideNest(ant.pos)) {
+  //     colony.addAnt(ant);
+  //     j++;
+  //   }
+  // }
 }
 
 
 function draw() {
   background(255);
   colony.run();
-  nest.render();
+  // nest.render();
   for (var i = 0; i < supply.length; i++) {
     supply[i].render();
   }
