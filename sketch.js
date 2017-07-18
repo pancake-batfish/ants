1089e238e370160ef14109fdf791e46066eef950
Regan Huff <regan.huff@gmail.com>
2017-07-18 14:50:34 -0700

    Food not distributed in nest

--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
 var colony;
-// var nest;
 var supply = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colony = new Colony();
   colony.establish();
+  var nest = colony.getNest();
 
   //distribution of food
   for (var i = 0; i < 15; i++) {
     var food = new Food(random(width), random(height));
     // if (food.pos.dist(nest.pos) > (nest.radius)) {
+    if (!nest.insideNest(food.pos)) {
       supply.push(food);
-    // }
+    }
   }
 
 }
