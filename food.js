298945b047ea30edc9c3d6f20a906ec29faf1375
Regan Huff <regan.huff@gmail.com>
2017-07-22 12:51:43 -0700

    Give food objects a unique id

--- food.js
+++ food.js
@@ -1,9 +1,18 @@
 function Food(x,y) {
   this.pos = createVector(x, y);
 
+  this.newUUID = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  };
+
+  this.id = this.newUUID();
+
   this.run = function() {
     this.render();
-  }
+  };
 
   this.render = function() {
     var lightGreen = color(113, 193, 79);
@@ -11,5 +20,6 @@ function Food(x,y) {
     strokeWeight(5);
     point(this.pos.x, this.pos.y);
     strokeWeight(1);
-  }
+  };
+
 }
